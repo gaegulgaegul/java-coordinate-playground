1d3b3d784f69cf305b736bfb698739c2825b9eb9
임명섭 <lms@elonsoft.co.kr>
2022-06-23 21:13:45 +0900

    feat 계산 값 formatting

--- src/main/java/coordinatecalculator/view/ResultView.java
+++ src/main/java/coordinatecalculator/view/ResultView.java
@@ -7,6 +7,14 @@ public class ResultView {
     }
 
     public static void printDistance(String comment, double distance) {
+        if (isInt(distance)) {
+            System.out.println(comment + " " + String.format("%.0f", distance));
+            return;
+        }
         System.out.println(comment + " " + String.format("%.6f", distance));
+    }
+
+    private static boolean isInt(double number){
+        return Math.ceil(number) == Math.floor(number);
     }
 }
