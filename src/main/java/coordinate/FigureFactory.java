d2ea990b6e58d80f88dd7734f638d5189a843003
임명섭 <lms@elonsoft.co.kr>
2022-06-25 22:11:20 +0900

    feat 다형성 제거 실습

--- src/main/java/coordinate/FigureFactory.java
+++ src/main/java/coordinate/FigureFactory.java
@@ -1,21 +1,39 @@
 package coordinate;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class FigureFactory {
-    static Figure getInstance(List<Point> points) {
-        if (points.size() == Line.LINE_POINT_SIZE) {
-            return new Line(points);
+public class FigureFactory implements FigureCreator {
+
+    private static Map<Integer, Class<? extends Figure>> factory = new HashMap<>();
+
+    static {
+        factory.put(Line.LINE_POINT_SIZE, Line.class);
+        factory.put(Triangle.TRIANGLE_POINT_SIZE, Triangle.class);
+        factory.put(Rectangle.RECTANGLE_POINT_SIZE, Rectangle.class);
+    }
+
+    @Override
+    public Figure create(List<Point> points) {
+        Class<? extends Figure> figureClass = factory.get(points.size());
+        if (figureClass == null) {
+            throw new IllegalArgumentException("유효하지 않은 도형입니다.");
         }
 
-        if (points.size() == Triangle.TRIANGLE_POINT_SIZE) {
-            return new Triangle(points);
-        }
+        return newInstance(points, figureClass);
 
-        if (points.size() == Rectangle.RECTANGLE_POINT_SIZE) {
-            return new Rectangle(points);
-        }
+    }
 
-        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
+    private Figure newInstance(List<Point> points, Class<? extends Figure> figureClass) {
+        try {
+            return figureClass.getConstructor(List.class).newInstance(points);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("List 타입의 매개변수를 가지는 생성자 메서드가 없습니다.");
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new IllegalArgumentException("새로운 인스턴스를 생성할 수 없습니다.");
+        }
     }
 }
--- src/test/java/coordinate/FigureFactoryTest.java
+++ src/test/java/coordinate/FigureFactoryTest.java
@@ -1,5 +1,7 @@
 package coordinate;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,13 +10,21 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FigureFactoryTest {
+
+    private FigureCreator figureCreator;
+
+    @BeforeEach
+    void beforeEach() {
+        this.figureCreator = new FigureFactory();
+    }
+
     @Test
     public void line() {
         List<Point> points = Arrays.asList(
                 Point.of(1, 2),
                 Point.of(2, 3));
 
-        Figure figure = FigureFactory.getInstance(points);
+        Figure figure = figureCreator.create(points);
         assertThat(figure).isInstanceOfAny(Line.class);
         assertThat(figure.getName()).isEqualTo("선");
     }
@@ -26,7 +36,7 @@ public class FigureFactoryTest {
                 Point.of(4, 1),
                 Point.of(1, 4));
 
-        Figure figure = FigureFactory.getInstance(points);
+        Figure figure = figureCreator.create(points);
         assertThat(figure).isInstanceOfAny(Triangle.class);
         assertThat(figure.getName()).isEqualTo("삼각형");
     }
@@ -39,7 +49,7 @@ public class FigureFactoryTest {
                 Point.of(1, 4),
                 Point.of(4, 4));
 
-        Figure figure = FigureFactory.getInstance(points);
+        Figure figure = figureCreator.create(points);
         assertThat(figure).isInstanceOfAny(Rectangle.class);
         assertThat(figure.getName()).isEqualTo("사각형");
     }
