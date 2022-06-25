d5e83b3f5c56b7f97cfdaf689ea54dd84b2b5c49
임명섭 <lms@elonsoft.co.kr>
2022-06-25 21:43:11 +0900

    feat 삼각형 넓이 계산

--- src/main/java/coordinatecalculator/domain/Triangle.java
+++ src/main/java/coordinatecalculator/domain/Triangle.java
@@ -8,7 +8,49 @@ public class Triangle extends Shape {
 
     @Override
     public double calculate() {
-        return 0;
+        double[] sideOfLengths = getSideOfLengths();
+        double covariate = getCovariate(sideOfLengths);
+        double area = getArea(sideOfLengths, covariate);
+        return Math.round(area);
+    }
+
+    private double getArea(double[] sideOfLengths, double covariate) {
+        double[] temporaries = new double[3];
+        for (int i = 0; i < sideOfLengths.length; i++) {
+            temporaries[i] = covariate - sideOfLengths[i];
+        }
+
+        double temporary = covariate;
+        for (double value : temporaries) {
+            temporary *= value;
+        }
+        return Math.sqrt(temporary);
+    }
+
+    private double getCovariate(double[] sideOfLengths) {
+        double summary = 0;
+        for (double sideOfLength : sideOfLengths) {
+            summary += sideOfLength;
+        }
+        return summary / 2;
+    }
+
+    private double[] getSideOfLengths() {
+        double[] sideOfLengths = new double[3];
+
+        for (int i = 0; i < 3; i++) {
+            int target = getTarget(i);
+            sideOfLengths[i] = positions.length(i, target);
+        }
+        return sideOfLengths;
+    }
+
+    private int getTarget(int i) {
+        int target = i + 1;
+        if (target == 3) {
+            target = 0;
+        }
+        return target;
     }
 
     @Override
--- src/test/java/coordinatecalculator/domain/TriangleTest.java
+++ src/test/java/coordinatecalculator/domain/TriangleTest.java
@@ -1,7 +1,22 @@
 package coordinatecalculator.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class TriangleTest {
 
+    @Test
+    @DisplayName("삼각형의 넓이 구하기")
+    void get_triangle_area() {
+        List<Position> positionList = Arrays.asList(new Position(10, 10),
+                new Position(14, 15), new Position(20, 8));
+        Positions positions = new Positions(positionList);
+        Triangle triangle = new Triangle(positions);
+        assertThat(triangle.calculate()).isEqualTo(29.0);
+    }
 }
